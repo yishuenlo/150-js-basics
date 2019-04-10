1df765f637243167f95e2cf32cd14f049d347e0f
Yishuen Lo <lo.yishuen@gmail.com>
2019-04-09 20:21:09 -0600

    selection from navbar
    change section border color when its selected through navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 //automatically create navigation list based on sections
 const NAV_LIST  = document.querySelector("#nav-list");
 const SECTIONS = document.querySelectorAll("section h3 a");
+const TILES = document.querySelectorAll("section");
 
-for (let i = 0; i <= SECTIONS.length; i++){
+for (let i = 0; i <= SECTIONS.length-1; i++){
   var newLi = document.createElement("li");
   var newAnchor = document.createElement("a");
 
@@ -23,3 +24,26 @@ for (let i = 0; i <= SECTIONS.length; i++){
   //insert li tag within ul
   NAV_LIST.appendChild(newLi);
 }
+
+
+//change border color when section is selected
+var currentAddress;
+var focusedAddress;
+
+window.addEventListener("hashchange", function(){
+  currentAddress = window.location.hash;
+
+  for(let i = 0; i <= TILES.length-1; i++){
+    focusedAddress = `#ex${SECTIONS[i].innerHTML.slice(0,3)}`
+
+    //check for url address
+    //highlights border then fades - using CSS
+    if(currentAddress == focusedAddress){
+      TILES[i].style.borderColor = "#FFB300";
+      TILES[i].style.borderWidth = "2px";
+    } else {
+      TILES[i].style.borderColor = "rgba(131, 131, 131, 0.2)";
+      TILES[i].style.borderWidth = "1px";
+    }
+  }
+})
