955b32ecb2570c8b6732b7138feb36885c5d2fe4
Yishuen Lo <lo.yishuen@gmail.com>
2019-04-22 07:11:51 -0600

    refine UI interaction with button

--- js/ex008.js
+++ js/ex008.js
@@ -1,64 +1,66 @@
-// Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched".
-
 const DISPLAY_RANDOM_NUM = document.querySelector("#computer-message"); //computer message
 const USER_NUM_GUESS = document.querySelector(".user-input input"); //user input
-const PREVIOUS_GUESSES = document.querySelectorAll(".previous-guess-visual span"); // list of guesses
-const RESET_RANDOM_NUM = document.querySelector("#reset-random-num"); //button
+const PREVIOUS_GUESS = document.querySelector(".previous-guess span");
+const RESET_RANDOM_NUM = document.querySelector(".reset-random-num"); //button
 
-var randomNum;
+var randomNum = 5;
 var inputNum;
 
 //generate a random number
 genRandomNum();
 
 //listen to enter key, and grab user's input from USER_NUM_GUESS
-USER_NUM_GUESS.addEventListener('keyup', function(e){
-  if (e.keyCode == 13){
+USER_NUM_GUESS.addEventListener('keyup', function(e) {
+  if (e.keyCode == 13) {
 
     inputNum = Number(this.value);
 
-    if (inputNum <= 10){
+    if (inputNum <= 10) {
       guessRandomNum(inputNum);
-      PREVIOUS_GUESSES[0].innerHTML = inputNum;
+      PREVIOUS_GUESS.innerHTML = inputNum;
     } else {
-      DISPLAY_RANDOM_NUM.innerHTML =  "Pick a number between 1 & 10";
+      DISPLAY_RANDOM_NUM.innerHTML = "Pick a number between 1 & 10";
     }
 
   }
 })
 
-
-USER_NUM_GUESS.addEventListener('keypress', function(){
+USER_NUM_GUESS.addEventListener('keypress', function() {
   DISPLAY_RANDOM_NUM.innerHTML = "press ENTER to guess";
 })
 
-
 // compaure USER_NUM_GUESS with randomNum
-function guessRandomNum(inputNum){
-  if (randomNum == inputNum){
+function guessRandomNum(inputNum) {
+  if (randomNum == inputNum) {
     DISPLAY_RANDOM_NUM.innerHTML = "GOOD WORK!";
     USER_NUM_GUESS.disabled = true;
-  } else if (randomNum > inputNum){
+    RESET_RANDOM_NUM.classList.remove("reset-random-num")
+    RESET_RANDOM_NUM.classList.add("reset-random-num-highlight");
+  } else if (randomNum > inputNum) {
     DISPLAY_RANDOM_NUM.innerHTML = "Not matched. Number too low.";
   } else {
     DISPLAY_RANDOM_NUM.innerHTML = "Not matched. Number too high.";
   }
 }
 
-// record USER_NUM_GUESS and display it in PREVIOUS_GUESSES
-
 
 // reset game when clicked on button
-RESET_RANDOM_NUM.addEventListener('mouseup', function(){
+RESET_RANDOM_NUM.addEventListener('mouseup', function() {
   //generate a random number
   genRandomNum();
 
+  // reset input and buttons
   USER_NUM_GUESS.disabled = false;
+  RESET_RANDOM_NUM.classList.remove("reset-random-num-highlight");
+  RESET_RANDOM_NUM.classList.add("reset-random-num")
 
+  //reset HTML display
   DISPLAY_RANDOM_NUM.innerHTML = "GUESS A NUMBER!"
+  PREVIOUS_GUESS.innerHTML = "";
 })
 
-function genRandomNum(){
+//generate a random number
+function genRandomNum() {
   USER_NUM_GUESS.value = "";
   randomNum = Math.floor(Math.random() * 10)
 }
