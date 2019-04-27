0c3042037c94fb9abdac605aaa0d7ae9833c248a
Yishuen Lo <lo.yishuen@gmail.com>
2019-04-27 09:23:10 -0600

    update calculation math
    clean up code to make it DRY

--- js/ex009.js
+++ js/ex009.js
@@ -6,22 +6,29 @@ const XMAS_HR = document.querySelector("#xmas-hr");
 const XMAS_MIN = document.querySelector("#xmas-min");
 const XMAS_SEC = document.querySelector("#xmas-sec");
 
-// Date().getTime() is in milliseconds, use secnod as calculation base.
-var secLeft = Math.ceil((xmas.getTime() - xmasToday.getTime())/1000);
-
-const ONE_DAY = 24 * 60 * 60; //one day = 86400 seconds
-const ONE_HOUR = 60 * 60; // one hour = 3600 seconds
-const ONE_MINUTE = 60; //one minute = 60 seconds
-
 // if today is after 12/25, add another year
 if (xmasToday.getMonth() == 11 && today.getDate() > 25){
   xmas.setFullYear(xmas.getFullYear() + 1);
 }
 
-var dayLeft = Math.floor(secLeft / ONE_DAY);
-var hourLeft = Math.floor((secLeft / ONE_DAY - dayLeft)*60);
-var minLeft = Math.floor(((secLeft / ONE_DAY - dayLeft)*60-hourLeft)*60);
+const ONE_DAY = 24 * 60 * 60 * 1000; //one day = 86,400,000 milliseconds
 
-XMAS_DAYS.innerHTML = dayLeft;
-XMAS_HR.innerHTML = hourLeft;
-XMAS_MIN.innerHTML = minLeft;
+// Date().getTime() is in milliseconds, use days as calculation base.
+var xmasCountdown = Math.ceil((xmas.getTime() - xmasToday.getTime())); // in milliseconds
+
+//convert milliseconds to days
+var dayLeftDecimal = xmasCountdown / ONE_DAY;
+var dayLeftInteger = Math.floor(dayLeftDecimal);
+
+//convert days decimals to hours
+var hourLeftDecimal = (dayLeftDecimal - dayLeftInteger)*24;
+var hourLeftInteger = Math.floor(hourLeftDecimal);
+
+//convert hours decimals to minutes
+var minLeftDecimal = (hourLeftDecimal - hourLeftInteger) * 60;
+var minLeftInteger = Math.floor(minLeftDecimal);
+
+
+XMAS_DAYS.innerHTML = dayLeftInteger;
+XMAS_HR.innerHTML = hourLeftInteger;
+XMAS_MIN.innerHTML = minLeftInteger;
