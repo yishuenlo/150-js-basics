3e02dc5c9674c5803600112e6457f0afeb4abf17
Yishuen Lo <lo.yishuen@gmail.com>
2019-09-20 19:26:31 -0600

    javascript calc
    listen to inputs and perform temp conversion

--- js/ex012.js
+++ js/ex012.js
@@ -2,12 +2,37 @@
 // C = (5/9) * (F - 32)
 // F = C * (9/5) + 32
 
+const FAHRENHEIT_INPUT = document.querySelector("#fahrenheit");
+const CELSIUS_INPUT = document.querySelector("#celsius");
+
+// listen to F keyup, then run convertToCelsius function after 800 ms
+FAHRENHEIT_INPUT.addEventListener('keyup', function(){
+  setTimeout(convertToCelsius, 800);
+})
+
+// listen to C keyup, then run convertToFahrenheit function after 800 ms
+CELSIUS_INPUT.addEventListener('keyup', function(){
+  setTimeout(convertToFahrenheit, 800);
+})
+
 function convertToCelsius(fa){
+  fa = Number(FAHRENHEIT_INPUT.value);
   let c = Math.floor((5/9) * (fa - 32));
-  return `${fa} degrees Fahrenheit is ${c} degrees Celsius`;
+  return CELSIUS_INPUT.value = c;
 }
 
 function convertToFahrenheit(cel){
+  cel = Number(CELSIUS_INPUT.value);
   let fa = Math.floor(cel * (9/5) + 32);
-  return `${cel} degrees Celsius is ${fa} degrees Fahrenheit`;
+  return FAHRENHEIT_INPUT.value = fa;
 }
+
+// function resetFahrenheitInput(){
+//   FAHRENHEIT_INPUT.value = "";
+//   clearTimeout(convertToCelsius);
+// }
+//
+// function resetCelsiusInput(){
+//   CELSIUS_INPUT.value = "";
+//   clearTimeout(convertToFahrenheit);
+// }
